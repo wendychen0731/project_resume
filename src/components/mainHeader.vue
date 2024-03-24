15a77ddee18a49429edea53e73979cb05192161d
DESKTOP-MN8CP5R\User <sun609099@gmail.com>
2024-03-24 13:37:51 +0800

    調整頁面架構

---
+++ src/components/mainHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="mynav">
+      <ul>
+        <li v-for="nav in navList"> {{ nav }}</li>
+    </ul>
+  </div>
+    
+  </header>
+  <RouterView />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navList:['About me', 'Resume', 'Projects', 'Contact'],
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+.mynav{
+  width: 100%;
+  height: 60px;
+  border: 1px solid #000;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
+      name: 'aboutme',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutMe.vue')
+    },
+    {
+      path: '/resume',
+      name: 'resume',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Resume.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Projects.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Contact.vue')
     }
   ]
 })
